3b69f81e1354c2d3fbf1bde49f96474538a47985
Marlon Falcón Hernández <falconsoft.3d@gmail.com>
2018-06-25 02:22:25 -0400

    Add files via upload

---
+++ default_analytic_account/models/__init__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    ODOO, Open Source Management Solution
+#    Copyright (C) 2016 Steigend IT Solutions
+#    For more details, check COPYRIGHT and LICENSE files
+#
+##############################################################################
+import partner
+import product
+import sale_order_line
+import purchase_order_line
+import account_invoice_line
